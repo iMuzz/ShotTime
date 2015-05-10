e3945aecbe48fc5c0a9bb5617a4fef3cef0e89ce
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-05-09 17:05:17 -0700

    grab device token from params and send notification

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,10 @@ class WelcomeController < ApplicationController
   def shot_time
     puts 'does this work?'
     # Grab Device Token
+    APNS.send_notification('754047bb1b148acb74f35d0710f7524e5df04b981e23d93849e747215ec736fa', 'Its Shot Time!' )
+  end
+
+  def take_shot
+    APNS.send_notification(params[:device_token], "It's time to take a shot!")
   end
 end
