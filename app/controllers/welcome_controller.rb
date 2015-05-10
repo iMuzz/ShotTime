3e455414b0144f270a77b1a6c2ca60570422a9f0
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-05-09 18:43:47 -0700

    Fix CSRF Token error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def shot_time_test
     APNS.send_notification('754047bb1b148acb74f35d0710f7524e5df04b981e23d93849e747215ec736fa', :alert => "It's beer time!", :sound => 'default')
     APNS.send_notification('87efab513ff67aa9fc79e7c39aa11c57b30c286432f9c6b042b02b6d36218868', :alert => "It's beer time!", :sound => 'default')
@@ -7,12 +9,12 @@ class WelcomeController < ApplicationController
   end
 
   def shot_time
-    APNS.send_notification(params[:device_token], "It's time to take a shot!")
+    APNS.send_notification(params[:device_token], :alert => "It's time to take a shot!", :sound => 'default')
     render nothing: true
   end
 
   def beer_time
-    APNS.send_notification(params[:device_token], "It's beer time!")
+    APNS.send_notification(params[:device_token], :alert => "It's beer time!", :sound => 'default')
     render nothing: true
   end
 end
